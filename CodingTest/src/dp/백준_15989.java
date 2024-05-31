a3ca9dead9db8f9467d5d25f7053ec12b942ef5e
Ji Eunyoung <jeunyeong323@gmail.com>
2024-05-31 16:14:07 +0900

    4주차 5

---
+++ CodingTest/src/dp/백준_15989.java
@@ -0,0 +1,40 @@
+package dp;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class 백준_15989 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int T = Integer.parseInt(br.readLine());
+		
+		int[][] dp = new int[10001][4];
+		
+		dp[1][1] = 1; // (1) 1가지
+		//dp[1][2] 
+		//dp[1][3]
+		dp[2][1] = 1; // (1+1) 1가지
+		dp[2][2] = 1; // (2) 1가지
+		//dp[2][3]
+		dp[3][1] = 1; // (1+1+1) 1가지
+		dp[3][2] = 1; //(1+2) 1가지
+		dp[3][3] = 1; //(3) 1가지
+		
+		for(int i = 0; i < T; i++) {
+			int n = Integer.parseInt(br.readLine());
+			
+			for(int j = 4; j <= 10000; j++) {
+				dp[j][1] = dp[j - 1][1];
+				dp[j][2] = dp[j - 2][1] + dp[j - 2][2];
+				dp[j][3] = dp[j - 3][1] + dp[j - 3][2] + dp[j - 3][3];
+			}
+			
+			System.out.println(dp[n][1] + dp[n][2] + dp[n][3]);
+		}
+
+	}
+
+}
